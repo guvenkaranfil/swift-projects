995873ad25887c12b7bf53cf07441bcf86c4996a
guvenkaranfil <guvenkaranfil235@gmail.com>
2020-12-20 02:07:44 +0300

    feat: set up xcode for project5

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Güven Karanfil on 20.12.2020.
+//  Copyright © 2020 Güven Karanfil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
