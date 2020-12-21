ced0c2a73193346d86dbb2e083809e485ab73e83
guvenkaranfil <guvenkaranfil235@gmail.com>
2020-12-21 03:48:06 +0300

    feat: project7 completed with its challanges

---
+++ Project7/Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  Project7
+//
+//  Created by Güven Karanfil on 21.12.2020.
+//  Copyright © 2020 Güven Karanfil. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
