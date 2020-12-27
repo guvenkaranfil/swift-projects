4603ae03ab60b5d5f7e68b69b391c8d9b7e55ff1
guvenkaranfil <guvenkaranfil235@gmail.com>
2020-12-28 02:42:03 +0300

    feat: project 10 completed

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Güven Karanfil on 28.12.2020.
+//  Copyright © 2020 Güven Karanfil. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
