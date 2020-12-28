3908e60248f2f97bdd656e51de1a52f40abdc0f8
guvenkaranfil <guvenkaranfil235@gmail.com>
2020-12-28 06:04:52 +0300

    feat: project 12a save data locall wtih NSCoding protocol

---
+++ Project12b/Project10/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Güven Karanfil on 28.12.2020.
+//  Copyright © 2020 Güven Karanfil. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
