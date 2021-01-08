1acce3583f1039aa2edc636cec2deb9ac87055ba
guvenkaranfil <guvenkaranfil235@gmail.com>
2021-01-08 05:26:53 +0300

    feat: project 19 completed

---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Güven Karanfil on 4.01.2021.
+//  Copyright © 2021 Güven Karanfil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
